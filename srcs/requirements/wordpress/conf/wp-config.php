b8c258c31087a43af6c2f7e60dbf6f2abd624790
coolasfk <fk.przybyla@gmail.com>
2024-12-01 16:57:28 +0100

    trying to solve cert ssl issues

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -5,13 +5,8 @@ define( 'DB_PASSWORD', 'evapass' );
 define( 'DB_HOST', 'mariadb:3306' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
-define('WP_DEBUG', true);
-define('WP_DEBUG_LOG', true);
-define('WP_DEBUG_DISPLAY', true);
 
 $table_prefix = 'wp_';
-
-define( 'WP_DEBUG', false );
 
 if ( ! defined( 'ABSPATH' ) ) {
     define( 'ABSPATH', __DIR__ . '/' );
