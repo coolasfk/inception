ef85bf89cbc6c6fbfc1f667a6c83378ac3107811
coolasfk <fk.przybyla@gmail.com>
2024-12-03 16:38:24 +0100

    env var not working inside wp-config.php

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,10 +1,11 @@
 <?php
-define( 'DB_NAME', 'wordpress' );
-define( 'DB_USER', 'wordpress' );
-define( 'DB_PASSWORD', 'evapass' );
-define( 'DB_HOST', 'mariadb:3306' );
+define( 'DB_NAME', 'wordpress');
+define( 'DB_USER', 'wordpress');
+define( 'DB_PASSWORD', 'evapass');
+define( 'DB_HOST', 'mariadb:3306');
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
+
 
 $table_prefix = 'wp_';
 
