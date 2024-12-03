5b5a07c20727d3bfe3ab046d9e9261cd81776e0d
coolasfk <fk.przybyla@gmail.com>
2024-12-04 00:50:09 +0100

    environmental variables working in php

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,16 +1,30 @@
 <?php
-define( 'DB_NAME', 'wordpress');
-define( 'DB_USER', 'wordpress');
-define( 'DB_PASSWORD', 'evapass');
-define( 'DB_HOST', 'mariadb:3306');
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
 
+// Database settings
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_CHARSET', getenv('DB_CHARSET') ?: 'utf8');
+define('DB_COLLATE', getenv('DB_COLLATE') ?: '');
 
-$table_prefix = 'wp_';
+// Authentication Unique Keys and Salts
+define('AUTH_KEY', getenv('AUTH_KEY'));
+define('SECURE_AUTH_KEY', getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY', getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY', getenv('NONCE_KEY'));
+define('AUTH_SALT', getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT', getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT', getenv('NONCE_SALT'));
 
-if ( ! defined( 'ABSPATH' ) ) {
-    define( 'ABSPATH', __DIR__ . '/' );
+// Database table prefix
+$table_prefix = getenv('TABLE_PREFIX') ?: 'wp_';
+
+// Absolute path to the WordPress directory
+if (!defined('ABSPATH')) {
+    define('ABSPATH', __DIR__ . '/');
 }
 
+// Sets up WordPress vars and included files
 require_once ABSPATH . 'wp-settings.php';
